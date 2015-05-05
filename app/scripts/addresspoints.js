df992584c3a3a1b53a5e09d679834effb6bc088f
kenTrinh <hdk.trinh.2012@smu.edu.sg>
2015-05-05 17:57:46 +0800

    First Commit

---
+++ app/scripts/addresspoints.js
@@ -0,0 +1,18 @@
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+[1.30917,103.71740, "200"],
+[1.30895,103.71712, "20"],
+[1.30926,103.71786, "20"],
+[1.3087,103.7178, "20"],
+[1.3092,103.7146, "20"],
+[1.30925,103.71409, "20"],
+[1.30751,103.71380, "20"],
+[1.30599,103.71387, "20"],
+[1.30326,103.71421, "20"],
+[1.30057,103.71451, "20"],
+[1.30088,103.71612, "20"],
+[1.30088,103.71612, "20"],
+[1.30088,103.71612, "20"],
+[1.30088,103.71612, "20"],
+]
